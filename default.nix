39983554fa2b2a184fc726282f89c40d93ac3315
Teo Klestrup Röijezon <teo@nullable.se>
2016-01-19 08:29:26 +0100

    Fixed parsets hash, pegged against LTS haskell

--- default.nix
+++ default.nix
@@ -6,5 +6,5 @@ args@{ pkgs ? (import <nixpkgs> {}).pkgs
 , extraConfig ? ""
 }:
 pkgs.callPackage ./parsets-playground.nix ({
-    parsets = pkgs.haskell.packages.ghc784.callPackage ./parsets.nix {};
+    parsets = pkgs.haskell.packages.lts-2_22.callPackage ./parsets.nix {};
 } // args)
--- parsets.nix
+++ parsets.nix
@@ -7,7 +7,7 @@ mkDerivation {
   version = "0.1.0.0";
   src = fetchgit {
     url = "git://github.com/blocklandglass/parsets";
-    sha256 = "f2d1b52328885a30528ece02ce0872fb16affc5d3846c4e49f43c2e5f4d0229d";
+    sha256 = "0s7x8vga25xh2gjjr098g90d686x27r80q8mbcx4wwr1k7q2msaq";
     rev = "aa1dff2448555535b9bab703209aa13d077f0aee";
   };
   isLibrary = false;
