d13178dbfce1bdc02975bbcf1282fd9216543787
Teo Klestrup Röijezon <teo@nullable.se>
2016-05-22 10:31:09 +0200

    Updated parsets.nix to take into account that nix now pulls submodules

--- parsets.nix
+++ parsets.nix
@@ -7,7 +7,7 @@ mkDerivation {
   version = "0.1.0.0";
   src = fetchgit {
     url = "git://github.com/blocklandglass/parsets";
-    sha256 = "0s7x8vga25xh2gjjr098g90d686x27r80q8mbcx4wwr1k7q2msaq";
+    sha256 = "1xsq2yxvspdrhs1cydxd3zh25rlwqwshnhv3i49f6jix0ns2xn3a";
     rev = "aa1dff2448555535b9bab703209aa13d077f0aee";
   };
   isLibrary = false;
